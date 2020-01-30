647d3e56f5d127a41d0a83f31b66c591530ccd7e
Avynn Donaghe <avynn@donaghe.com>
2020-01-30 13:33:32 -0800

    Forgot to branch sorry!

---
+++ app/src/main/java/edu/wwu/csci412/SolveAndSnooze/Math_solve.java
@@ -0,0 +1,14 @@
+package edu.wwu.csci412.SolveAndSnooze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Math_solve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_solve);
+    }
+}
