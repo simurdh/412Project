64bd076149e2ae5a9af2240587762d5b592a9d9a
Conrad <conrad@Ethans-MacBook-Pro.local>
2020-03-05 22:20:18 -0800

    starting voice enabled commands

--- app/src/main/java/edu/wwu/csci412/SolveAndSnooze/EditAlarm.java
+++ app/src/main/java/edu/wwu/csci412/SolveAndSnooze/EditAlarm.java
@@ -1,24 +1,72 @@
 package edu.wwu.csci412.SolveAndSnooze;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /* edit alarm screen controller */
 public class EditAlarm extends AppCompatActivity {
+    private static final int COMMAND_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_alarm);
 
+        //Test if device supports speech recognition
+        PackageManager manager = getPackageManager();
+        List<ResolveInfo> listOfMatches = manager.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (listOfMatches.size() > 0) {
+            listen();
+        }
+        else {
+            //Speech recognition not supported
+            Toast.makeText(this, "Device does not support speech recognition", Toast.LENGTH_LONG).show();
+        }
     }
+
+    private void listen() {
+        Intent listenIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        listenIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say a command");
+        listenIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        listenIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+        startActivityForResult(listenIntent, COMMAND_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == COMMAND_REQUEST && resultCode == RESULT_OK) {
+            //get list of possible words
+            ArrayList<String> returnedWords = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            //get scores for possible words
+            float [] scores = data.getFloatArrayExtra(RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
+
+            //dipslay results
+            int i = 0;
+            for (String word: returnedWords) {
+                if (scores != null && i < scores.length) {
+                    Log.w("EditAlarm", word + ": " + scores[i]);
+                }
+                i++;
+            }
+        }
+    }
+
     public void onStart() {
         super.onStart();
         updateView();
